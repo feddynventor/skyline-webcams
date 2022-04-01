2e7fbfb26319fe4d8a81d4fb2ee93517c577adfe
fedele <allerta12@gmail.com>
2022-04-01 08:26:08 +0200

    Craw camera list per country

--- server.js
+++ server.js
@@ -15,6 +15,56 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/list/cam/:state', (req, res) => {
+
+    HTTPclient.get(`https://www.skylinewebcams.com/en/webcam/${req.params.state}.html`,
+    function (query) {
+        let list = []
+        if (query.statusCode!=200){
+            res.status(404).send()
+            return
+        }
+        let page_body = "";
+        query.on('data', function (chuck) {
+            page_body+=chuck
+        })
+        query.on('end', function (){
+            // console.log(HTMLParser.parse(page_body, {"blockTextElements": {"noscript":false,"style":false,"pre":false,"script":false}}).childNodes[1].childNodes[1].childNodes[1].childNodes[0])
+            // const pageroot = HTMLParser.parse(page_body, {"blockTextElements": {"noscript":false,"style":false,"pre":false,"script":false}}).childNodes[1].childNodes[1].childNodes[1].childNodes[0].childNodes[1]
+            const pageroot = HTMLParser.parse(page_body, {"blockTextElements": {"noscript":false,"style":false,"pre":false,"script":false}}).childNodes[1].childNodes[1].childNodes[1].childNodes[0].childNodes[0]
+            pageroot.childNodes.forEach(element => {
+                
+                let meta = {};
+                element.childNodes[0].childNodes.forEach(detail => {
+                    if (detail.rawAttrs == 'class="tcam"'){
+                        meta['title'] = detail.childNodes[0]._rawText
+                    }
+                    if (detail.rawAttrs == 'class="subt"'){
+                        meta['desc'] = detail.childNodes[0]._rawText
+                    }
+                    if (detail.rawAttrs == 'class="lcam"'){
+                        meta['tag'] = detail.childNodes[0]._rawText
+                    }
+                })
+                if (!Object.keys(meta).length) return;
+
+                let queryDet = element.rawAttrs.substring(element.rawAttrs.indexOf("en/webcam/")+10, element.rawAttrs.indexOf(".html")).split("/")
+
+                list.push({
+                    country: queryDet[0],
+                    region: queryDet[1],
+                    city: queryDet[2],
+                    place: queryDet[3],
+                    meta,
+                    // flag: `https://www.countryflagicons.com/SHINY/64/${countryCode}.png`
+                })
+            })
+            res.send(list)
+        })
+    })
+
+})
+
 app.get('/stream/:state/:region/:city/:place', (req, res) => {
 
     HTTPclient.get(`https://www.skylinewebcams.com/it/webcam/${req.params.state}/${req.params.region}/${req.params.city}/${req.params.place}.html`,
@@ -47,6 +97,7 @@ app.get('/stream/:state/:region/:city/:place', (req, res) => {
                 try {
                     scriptTag = pageroot.childNodes[1].childNodes[1].childNodes[6].childNodes[0]._rawText
 
+                    if (scriptTag.indexOf("videoId:'")==-1) { res.sendStatus(404); return; }
                     HTTPclient.get("https://www.youtube.com/watch?v="+scriptTag.substr(scriptTag.indexOf("videoId:'")+9,11),
                         function (query) {
                             if (query.statusCode!=200){
